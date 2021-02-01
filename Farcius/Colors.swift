5358ee58a01806fc23a5c39dded2e5b6c2f01f77
matejmecka <matej.plavevski+github@gmail.com>
2021-02-01 15:22:28 +0100

    Support for Dark Mode

---
+++ Farcius/Colors.swift
@@ -0,0 +1,26 @@
+//
+//  Colors.swift
+//  Farcius
+//
+//  Created by Svitlana Honcharuk on 30.01.2021.
+//
+
+import Foundation
+import UIKit
+
+class Colors {
+    
+    static var background: UIColor  {
+        return UIColor(named: "Background")!
+    }
+    
+    static var button: UIColor  {
+        return UIColor(named: "ButtonColor")!
+    }
+    
+    static var text: UIColor  {
+        return UIColor(named: "TextColor")!
+    }
+    
+    
+}
--- Farcius/Game.swift
+++ Farcius/Game.swift
@@ -13,67 +13,74 @@ struct Game: View {
     let MAX_INACCURACY = 4 // allowed inaccuracy, in Celsius
     @State var curTemperature = TemperatureGenerator.generateTemperature()
     
+    @Environment(\.colorScheme) var colorScheme
+    var colors = Colors()
+    
     var body: some View {
-        VStack(alignment: .center) {
-            Spacer()
-            Text("\(curTemperature.getFahrenheit())ºF is ")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-            
-            Spacer()
-            HStack(alignment: .center) {
+        ZStack {
+            VStack(alignment: .center) {
                 Spacer()
-                TextField("?", text: $inputTemp)
-                    .padding([.top, .leading, .bottom])
-                    .frame(width: 70.0)
-                    .font(.title)
-                    .accentColor(.black)
-                    .multilineTextAlignment(.trailing)
-                Text("ºC")
-                    .font(.title)
-                    .fontWeight(.bold)
+                Text("\(curTemperature.getFahrenheit())ºF is ")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                
+                Spacer()
+                HStack(alignment: .center) {
+                    Spacer()
+                    TextField("?", text: $inputTemp)
+                        .padding([.top, .leading, .bottom])
+                        .frame(width: 70.0)
+                        .font(.title)
+                        .accentColor(.black)
+                        .multilineTextAlignment(.trailing)
+                    
+            
+                    Text("ºC")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button(action: {
+                    let celsiusTemp = Int(inputTemp)
+                    if celsiusTemp != nil {
+                        let inaccuracy = celsiusTemp! - curTemperature.getCelsius()
+                        if abs(inaccuracy) < MAX_INACCURACY {
+                            result = "Great! It's \(curTemperature.getCelsius())ºC"
+                        } else {
+                            result = "Nope... It's \(curTemperature.getCelsius())ºC"
+                        }
+                    }
+                }, label: {
+                    Text(result)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 50.0)
+                        .padding(.vertical)
+                        .frame(width: nil)
+                        .background(Color(Colors.button))
+                        .foregroundColor(Color(Colors.text))
+                })
+                    .disabled(result != "Try")
+                Spacer()
+                
+                Button(action: {
+                    curTemperature = TemperatureGenerator.generateTemperature()
+                    result = "Try"
+                    inputTemp = ""
+                }, label: {
+                    Text("Next")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 50.0)
+                        .padding(.vertical)
+                        .background(Color(Colors.button))
+                        .foregroundColor(Color(Colors.text))
+                })
                 Spacer()
             }
-            Spacer()
-            
-            Button(action: {
-                let celsiusTemp = Int(inputTemp)
-                if celsiusTemp != nil {
-                    let inaccuracy = celsiusTemp! - curTemperature.getCelsius()
-                    if abs(inaccuracy) < MAX_INACCURACY {
-                        result = "Great! It's \(curTemperature.getCelsius())ºC"
-                    } else {
-                        result = "Nope... It's \(curTemperature.getCelsius())ºC"
-                    }
-                }
-            }, label: {
-                Text(result)
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .padding(.horizontal, 50.0)
-                    .padding(.vertical)
-                    .frame(width: nil)
-                    .background(Color.black)
-                    .foregroundColor(.white)
-            })
-                .disabled(result != "Try")
-            Spacer()
-            
-            Button(action: {
-                curTemperature = TemperatureGenerator.generateTemperature()
-                result = "Try"
-                inputTemp = ""
-            }, label: {
-                Text("Next")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .padding(.horizontal, 50.0)
-                    .padding(.vertical)
-                    .background(Color.black)
-                    .foregroundColor(.white)
-            })
-            Spacer()
-        }
+        }.preferredColorScheme(colorScheme).background(Color(Colors.background))
     }
 }
 
